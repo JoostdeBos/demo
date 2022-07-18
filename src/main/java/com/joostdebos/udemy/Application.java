d3dffc60faa1055539a2105d10f630981d3584c1
Joost de Bos <JoostdeBos>
2022-07-18 09:09:16 +0000

    Initial commit

---
+++ src/main/java/com/joostdebos/udemy/Application.java
@@ -0,0 +1,9 @@
+package com.joostdebos.udemy;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
